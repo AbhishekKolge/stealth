a5ac2b2c0d9e43095e4ab05882afca19f8697070
Abhishek Kolge <abhishek.kolge@soar.inc>
2025-07-20 02:22:05 +0530

    feat(init): project initialization

---
+++ packages/renderer/src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from "./components/ui/button";
+
+export const App = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <Button>Click me</Button>
+    </div>
+  );
+};
